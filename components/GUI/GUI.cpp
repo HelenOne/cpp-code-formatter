e31698154e07e0c0d9dcd20a16d1a41c0fd9f62b
HelenOne <h.bright@yandex.ru>
2020-06-05 02:53:46 +0300

    added interface

--- components/GUI/GUI.cpp
+++ components/GUI/GUI.cpp
@@ -5,6 +5,7 @@
 
 #include "../format/format.h"
 #include "../fileExist/fileExist.h"
+#include "./String.h"
 
 void GUI()
 {
@@ -32,14 +33,41 @@ void GUI()
   }
   system("cls");
 
-  char filenameToSave[256];
+  // Разделение названия файла на собственно название и расширение
+  String stringFileName;
+  stringFileName = filename;
+  String baseFileName;
+  String extensionName;
+  int index = 0;
+  for (int i = stringFileName.length() - 1; (stringFileName[i] != '.') && (i != 0); i--)
+  {
+    index = i;
+  }
+  for (int i = 0; i < index - 1; i++)
+  {
+    char buffer[2] = {stringFileName[i], '\0'};
+    baseFileName += buffer;
+  }
+  for (int i = index - 1; i < stringFileName.length(); i++)
+  {
+    char buffer[2] = {stringFileName[i], '\0'};
+    extensionName += buffer;
+  }
 
+  stringFileName = "";
+  stringFileName += baseFileName;
+  stringFileName += "_formatted";
+  stringFileName += extensionName;
+
+  char filenameToSave[256];
   std::wcout
       << L"Введите путь к файлу, в котором вы хотите сохранить отформатированный код"
-      << L" (или нажмите enter и код автоматически сохранится в "
-      << filename
-      << "):"
-      << std::endl;
+      << L" (или нажмите enter и код автоматически сохранится в ";
+  std::cout << baseFileName
+            << "_formatted"
+            << extensionName
+            << "):"
+            << std::endl;
 
   std::cin.getline(filenameToSave, 256, '\n');
   std::cout << std::endl;
@@ -48,7 +76,10 @@ void GUI()
 
   if (filenameToSave[0] == '\0')
   {
-    strcpy(filenameToSave, filename);
+    for (int i = 0; i < stringFileName.length(); i++)
+    {
+      filenameToSave[i] = stringFileName[i];
+    }
   }
 
   format(filename, filenameToSave);
